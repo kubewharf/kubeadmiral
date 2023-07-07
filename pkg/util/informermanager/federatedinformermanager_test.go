857a391e991a1aaa1719005e3fcde739aa9b4a6b
hawjia.lim <hawjia.lim@bytedance.com>
2023-07-07 14:18:26 +0800

    add bijection map to handle source type changes

---
+++ pkg/util/informermanager/federatedinformermanager_test.go
@@ -0,0 +1,19 @@
+package informermanager
+
+import "testing"
+
+func TestClientAvailableForExistingClusters(t *testing.T) {
+
+}
+
+func TestClientAvailableForNewCluster(t *testing.T) {
+
+}
+
+func TestListerAvailableForExistingFTCsAndClusters(t *testing.T) {
+
+}
+
+func TestListerAvailableForNewFTCAndCluster(t *testing.T) {
+
+}
