48ffcf76f83d81d41d0bef385b2180a7f372ea1a
miraclejzd <1061954832@qq.com>
2024-01-03 10:15:33 +0000

    add feat: admiralctl join&unjoin

--- pkg/admiralctl/admiralctl.go
+++ pkg/admiralctl/admiralctl.go
@@ -1,26 +1,11 @@
-/*
-Copyright 2023 The KubeAdmiral Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package admiralctl
 
 import (
 	"flag"
 	"fmt"
-	"github.com/kubewharf/kubeadmiral/pkg/admiralctl/federalize"
-	"github.com/kubewharf/kubeadmiral/pkg/admiralctl/options"
+
+	"github.com/kubewharf/kubeadmiral/pkg/admiralctl/join"
+	"github.com/kubewharf/kubeadmiral/pkg/admiralctl/unjoin"
 	"github.com/kubewharf/kubeadmiral/pkg/admiralctl/util"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -28,7 +13,6 @@ import (
 	apiserverflag "k8s.io/component-base/cli/flag"
 	"k8s.io/klog/v2"
 	"k8s.io/kubectl/pkg/util/templates"
-	"os"
 )
 
 var (
@@ -65,18 +49,19 @@ func NewDefaultAdmiralctlCommand() *cobra.Command {
 	// Prevent klog errors about logging before parsing.
 	_ = flag.CommandLine.Parse(nil)
 	f := util.NewFactory(defaultConfigFlags)
-	ioStreams := genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
+
+	// ioStreams := genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
 	groups := templates.CommandGroups{
 		{
 			Message: "Resource Management Commands:",
 			Commands: []*cobra.Command{
-				federalize.NewCmdFederalize(f, cliName),
+				join.NewCmdJoin(f, cliName),
+				unjoin.NewCmdJoin(f, cliName),
 			},
 		},
 	}
 	groups.Add(rootCmd)
-	filters := []string{"options"}
-	rootCmd.AddCommand(options.NewCmdOptions(cliName, ioStreams.Out))
+	filters := []string{""}
 	templates.ActsAsRootCommand(rootCmd, filters, groups...)
 
 	return rootCmd
--- pkg/admiralctl/util/factory.go
+++ pkg/admiralctl/util/factory.go
@@ -1,23 +1,8 @@
-/*
-Copyright 2023 The KubeAdmiral Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package util
 
 import (
 	"context"
+
 	fedclient "github.com/kubewharf/kubeadmiral/pkg/client/clientset/versioned"
 	"github.com/kubewharf/kubeadmiral/pkg/controllers/common"
 	clusterutil "github.com/kubewharf/kubeadmiral/pkg/util/cluster"
@@ -98,7 +83,7 @@ func NewFactory(kubeConfigFlags *genericclioptions.ConfigFlags) Factory {
 
 // NewClusterFactoryByKubeConfig create a new ClusterFactory by KubeConfig
 func NewClusterFactoryByKubeConfig(clusterKubeConfig, clusterContext string) (cmdutil.Factory, error) {
-	configFlags := genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag()
+	configFlags := genericclioptions.NewConfigFlags(true)
 	configFlags.KubeConfig = &clusterKubeConfig
 	configFlags.Context = &clusterContext
 	return cmdutil.NewFactory(configFlags), nil
