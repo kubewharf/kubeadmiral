73140d5dbd541bedeb956fd04034259a3d617ba4
shentiecheng <shentiecheng@bytedance.com>
2025-03-11 17:06:51 +0800

    feat：upgrade go mod

--- pkg/client/clientset/versioned/typed/core/v1alpha1/fake/fake_collectedstatus.go
+++ pkg/client/clientset/versioned/typed/core/v1alpha1/fake/fake_collectedstatus.go
@@ -8,7 +8,6 @@ import (
 	v1alpha1 "github.com/kubewharf/kubeadmiral/pkg/apis/core/v1alpha1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -20,9 +19,9 @@ type FakeCollectedStatuses struct {
 	ns   string
 }
 
-var collectedstatusesResource = schema.GroupVersionResource{Group: "core.kubeadmiral.io", Version: "v1alpha1", Resource: "collectedstatuses"}
+var collectedstatusesResource = v1alpha1.SchemeGroupVersion.WithResource("collectedstatuses")
 
-var collectedstatusesKind = schema.GroupVersionKind{Group: "core.kubeadmiral.io", Version: "v1alpha1", Kind: "CollectedStatus"}
+var collectedstatusesKind = v1alpha1.SchemeGroupVersion.WithKind("CollectedStatus")
 
 // Get takes name of the collectedStatus, and returns the corresponding collectedStatus object, and an error if there is any.
 func (c *FakeCollectedStatuses) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.CollectedStatus, err error) {
--- pkg/client/clientset/versioned/typed/core/v1alpha1/fake/fake_schedulingprofile.go
+++ pkg/client/clientset/versioned/typed/core/v1alpha1/fake/fake_schedulingprofile.go
@@ -8,7 +8,6 @@ import (
 	v1alpha1 "github.com/kubewharf/kubeadmiral/pkg/apis/core/v1alpha1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -19,9 +18,9 @@ type FakeSchedulingProfiles struct {
 	Fake *FakeCoreV1alpha1
 }
 
-var schedulingprofilesResource = schema.GroupVersionResource{Group: "core.kubeadmiral.io", Version: "v1alpha1", Resource: "schedulingprofiles"}
+var schedulingprofilesResource = v1alpha1.SchemeGroupVersion.WithResource("schedulingprofiles")
 
-var schedulingprofilesKind = schema.GroupVersionKind{Group: "core.kubeadmiral.io", Version: "v1alpha1", Kind: "SchedulingProfile"}
+var schedulingprofilesKind = v1alpha1.SchemeGroupVersion.WithKind("SchedulingProfile")
 
 // Get takes name of the schedulingProfile, and returns the corresponding schedulingProfile object, and an error if there is any.
 func (c *FakeSchedulingProfiles) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.SchedulingProfile, err error) {
